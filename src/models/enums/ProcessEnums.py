0c2291ecc6d780a588081427a0098c8d75dbcc00
ahmed khiari <khiari.ahmed2002@gmail.com>
2025-11-25 12:01:27 +0100

    manage document processing

---
+++ src/models/enums/ProcessEnums.py
@@ -0,0 +1,8 @@
+from enum import Enum 
+
+class ProcessEnum(Enum):
+    
+    TXT = ".txt"
+    PDF = ".pdf"
+    DOCX=".docx"
+    PPTX=".pptx"
--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -7,4 +7,6 @@ class ResponseSignal(Enum) :
     FILE_SIZE_EXCEEDED="file_size_exceeded"
     FILE_UPLOADED_SUCCESS="file_uploaded_success"
     FILE_UPLOADED_FAILED="file_uploaded_failed"
+    PROCESSING_FAILED="processing_failed"
+    PROCESSING_SUCCESS="processing_success"
     
